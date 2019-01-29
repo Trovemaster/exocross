e5e47c7ac1f01a352438f706ef48ea1d999609e7
Sergey Yurchenko <sergei.yurchenko@gmail.com>
2019-01-29 14:13:09 +0100

    bat

---
+++ exe/run_exocross.bat
@@ -0,0 +1,6 @@
+REM example of a Windows batch file
+REM Copy xcross_dos.exe to the folder containing the input
+REN Open the window containing the working folder and execute run_exocross.bat
+xcross_dos.exe < NaH_Voigt_1500K.inp> NaH_Voigt_1500K.out
+
+
